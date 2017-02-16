e1bd5b4eedb664d79dfc454c5ec33bdb57f0526d
buffalosauce37 <league@Mac-mini-2.local>
2017-02-15 18:26:09 -0800

    I finished whachamle and created SpellingBee

--- src/WhachaMole.java
+++ src/WhachaMole.java
@@ -1,19 +1,36 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 
 public class WhachaMole implements ActionListener {
 	JFrame frame = new JFrame();
-	
-	void DrawButtons(int mole) {
+	Date start = new Date();
+	int counter = 0;
+	void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
+	private void endGame(Date timeAtStart, int molesWhacked) {
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is " + 
+		((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) 
+		+ " moles per second.");
+	}
+	void DrawButtons(int mole) {
+		frame = new JFrame();
 		JPanel panel = new JPanel();
 		frame.add(panel);
-		
+
 		for (int i = 0; i < 24; i++) {
 			JButton button = new JButton();
 			panel.add(button);
@@ -35,10 +52,19 @@ public class WhachaMole implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		JButton but =  (JButton) e.getSource();
-		if(but.getText().equals("mole")){
+		JButton but = (JButton) e.getSource();
+		if (but.getText().equals("mole")) {
 			System.out.println("congrats");
 			frame.dispose();
+			int random = new Random().nextInt(24);
+			DrawButtons(random);
+			counter++;
+			if(counter==10){
+				endGame(start, counter);
+			}
+			
+		} else {
+			speak("WHAT A NERD!!!!!");
 			
 		}
 	}
